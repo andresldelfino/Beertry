e1c24e90d23fb468892e1430b86c38e6f368e859
andresldelfino <andresldelfino@gmail.com>
2023-03-18 13:24:00 -0300

    Add chequeos y validaciones
    Se agregan algunos chequeos y validaciones en las rutas de muestras y evaluaciones

--- routers/sample.js
+++ routers/sample.js
@@ -2,10 +2,18 @@ const express = require("express")
 const router = express.Router()
 const sampleController = require("../controllers/sampleController")
 const { validateSample } = require("../middleware/validateSample")
+const { check } = require("express-validator")
 
 
 router.get      ("/getall",                              sampleController.getAllSample)
-router.post     ("/setSample",                           sampleController.setSample)
+router.post     ("/setSample",          [
+    check("mail").not().isEmpty().withMessage("el campo mail es obligatorio").isEmail().withMessage("la direccion de email no tiene elformato adecuado"),
+    check("name").not().isEmpty().withMessage("el campo nombre es obligatorio"),
+    check("style").not().isEmpty().withMessage("el campo estilo es obligatorio"),
+    check("srm").not().isEmpty().withMessage("el campo color es obligatorio"),
+    check("abv").not().isEmpty().withMessage("el campo alcohol es obligatorio"),
+    check("category").not().isEmpty().withMessage("el campo ecategoria es obligatorio")
+]                                                      , sampleController.setSample)
 router.get      ("/getSampleById/:id",  validateSample , sampleController.getSampleById)
 router.put      ("/editSample/:id",     validateSample , sampleController.editSample)
 router.delete   ("/deleteSample/:id",   validateSample , sampleController.deleteSample)
--- routers/sampleTry.js
+++ routers/sampleTry.js
@@ -9,7 +9,17 @@ const { check } = require("express-validator")
 
 
 router.get("/getall"                                                        ,sampleTryController.getAllSampleTry)
-router.post("/setSampleTry",                  validateJugde, validateSample ,sampleTryController.setSampleTry)
+router.post("/setSampleTry", [
+    check("sample").not().isEmpty().withMessage("el campo muestra es obligatorio").isEmail().withMessage("la direccion de email no tiene elformato adecuado"),
+    check("judge").not().isEmpty().withMessage("el campo juez es obligatorio"),
+    check("turbidity").not().isEmpty().withMessage("el campo turbidez es obligatorio"),
+    check("srm").not().isEmpty().withMessage("el campo color es obligatorio"),
+    check("foam").not().isEmpty().withMessage("el campo espuma es obligatorio"),
+    check("retention").not().isEmpty().withMessage("el campo retencion de espuma es obligatorio"),
+    check("balance").not().isEmpty().withMessage("el campo balance es obligatorio"),
+    check("body").not().isEmpty().withMessage("el campo body es obligatorio")
+    
+]                                           , validateJugde, validateSample ,sampleTryController.setSampleTry)
 router.get("/getSampleTryById/:id",           validateSampleTry             ,sampleTryController.getSampleTryById)
 router.get("/getSampleTriesByJudge/:judge",   validateJugde                 ,sampleTryController.getSampleTriesByJudge)
 router.get("/getSampleTriesBySample/:sample", validateSample                ,sampleTryController.getSampleTriesBySample)
